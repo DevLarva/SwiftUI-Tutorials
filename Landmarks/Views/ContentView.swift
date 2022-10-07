a3a56b3334c0c8178a3d8aae04dc1de0319c9032
백대홍 <bdh3620@naver.com>
2022-10-07 16:57:59 +0900

    Building Lists and Navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 백대홍 on 2022/10/05.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
